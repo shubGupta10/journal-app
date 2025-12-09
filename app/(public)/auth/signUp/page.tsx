5289d504b7220876cb39076b229acd66267f3997
Shubham Kumar Gupta <shubhamkumargupta1012@gmail.com>
2025-12-09 17:58:20 +0530

    Added middleware support

--- app/(public)/auth/signUp/page.tsx
+++ app/(public)/auth/signUp/page.tsx
@@ -39,7 +39,7 @@ export default function SignUp() {
             callbackURL: '/login'
         } as any, {
             onSuccess: () => {
-                router.push('/login');
+                router.push('/auth/login');
             },
             onError: (ctx) => {
                 setLoading(false);
---
+++ proxy.ts
@@ -0,0 +1,26 @@
+import {NextResponse, NextRequest} from "next/server";
+
+export default function proxy(request: NextRequest) {
+    const {nextUrl} = request;
+    const path = nextUrl.pathname;
+
+    const publicRoutes = path === "/" || path.startsWith("/auth/login") || path.startsWith("/auth/signUp");
+
+    const sessionCookie = request.cookies.get('better-auth.session_token');
+
+    if(!publicRoutes && !sessionCookie){
+        return NextResponse.redirect(new URL("/auth/login", nextUrl));
+    }
+
+    if(publicRoutes && sessionCookie && (path.startsWith("/auth/login") || path.startsWith("/auth/signUp"))){
+        return NextResponse.redirect(new URL("/dashboard", nextUrl));
+    }
+
+    return NextResponse.next();
+}
+
+export const config = {
+    matcher: [
+        "/((?!api|_next/static|_next/image|favicon.ico).*)",
+    ],
+};
