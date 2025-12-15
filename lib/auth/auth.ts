2b31ca43c8da3f95e0ae45ed8fa4a5527f8ffa3d
Shubham Kumar Gupta <shubhamkumargupta1012@gmail.com>
2025-12-15 19:51:26 +0530

    fixed middleware issue

--- lib/auth/auth.ts
+++ lib/auth/auth.ts
@@ -1,21 +1,25 @@
-import {betterAuth} from 'better-auth'
-import {mongodbAdapter} from 'better-auth/adapters/mongodb'
+import { betterAuth } from 'better-auth'
+import { mongodbAdapter } from 'better-auth/adapters/mongodb'
 import clientPromise from "@/lib/db/mongoClient";
-import {username} from "better-auth/plugins";
+import { username } from "better-auth/plugins";
 
 const client = await clientPromise;
 const db = client.db("journal-app");
 
 export const auth = betterAuth({
     database: mongodbAdapter(db),
+    trustedOrigins: [
+        "http://localhost:3000",
+        process.env.NEXT_PUBLIC_BETTER_AUTH_URL || "http://localhost:3000",
+    ],
     emailAndPassword: {
         enabled: true
     },
     socialProviders: {
-       google: {
-           clientId: process.env.GOOGLE_CLIENT_ID as string,
-           clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
-       }
+        google: {
+            clientId: process.env.GOOGLE_CLIENT_ID as string,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+        }
     },
     plugins: [
         username()
